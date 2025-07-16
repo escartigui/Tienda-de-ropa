fc7c0d3f1da5f8ab26abe23346fa6f5f7654dc72
escartigui <escarlethmayali@gmail.com>
2025-07-15 19:47:23 -0600

    escar

--- tienda de ropa.py
+++ tienda de ropa.py
@@ -1,2 +1,9 @@
 tienda= []
 cantidad = int(input(f"\nIngrese la cantidad de sus productos: "))
+for i in range(cantidad):
+    print(f"\nproductos #{i+1}")
+
+    while true:
+        codigo = input(f"\nIngrese el codigo del producto: ")
+        if codigo in tienda:
+            print("codigo ya utilizado")
