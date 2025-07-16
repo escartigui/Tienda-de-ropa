98c280db1d72bdf5e12a8ba4c7a0da174efb235c
escartigui <escarlethmayali@gmail.com>
2025-07-15 21:25:00 -0600

    escar

--- tienda de ropa.py
+++ tienda de ropa.py
@@ -10,31 +10,32 @@ for i in range(cantidad):
         else:
             break
 
-nombre = input("Nombre del Producto")
-categoria = input("Categoria (hombre,mujer,niño)")
-talla = input("Talla (XS,S,M,L,XL)")
+    nombre = input("Nombre del Producto ")
+    categoria = input("Categoria (hombre,mujer,niño) ")
+    talla = input("Talla (XS,S,M,L,XL) ")
 
-while True:
- precio = int(input("ingrese el precio del producto"))
- if precio <= 0:
-    print("Verifica lo ingresado, no puede ser negativo")
- else:
-    break
-
-while True:
-    stock = int(input(f"\nIngrese la cantidad los producto: "))
-    if stock <= 0:
+    while True:
+       precio = int(input("ingrese el precio del producto"))
+       if precio <= 0:
         print("Verifica lo ingresado, no puede ser negativo")
-    else:
-        break
+       else:
+         break
 
-tienda[codigo] = {
-    'nombre': nombre,
-    'categoria': categoria,
-    'talla': talla,
-    'precio': precio,
-    'stock': stock,
+    while True:
+        stock = int(input(f"\nIngrese la cantidad los producto: "))
+        if stock <= 0:
+         print("Verifica lo ingresado, no puede ser negativo")
+        else:
+         break
+
+    tienda[codigo] = {
+        "nombre": nombre,
+        "categoria": categoria,
+        "talla": talla,
+        "precio": precio,
+        "stock": stock,
 }
+
 print("\nLista de estudiantes: ")
 for codigo,datos in tienda.items():
     print(f"codigo: {codigo}")
@@ -54,4 +55,5 @@ if encontrado in tienda:
     print(f"stock: {tienda[encontrado]['stock']}")
 else:
     print("No se encuentra el producto")
+
 
