914c5f54d83c10e7ff7b1b39b5a3202a8a22aab7
escartigui <escarlethmayali@gmail.com>
2025-07-15 19:51:34 -0600

    escar

--- tienda de ropa.py
+++ tienda de ropa.py
@@ -3,7 +3,12 @@ cantidad = int(input(f"\nIngrese la cantidad de sus productos: "))
 for i in range(cantidad):
     print(f"\nproductos #{i+1}")
 
-    while true:
+    while True: #este servira como mi verificador
         codigo = input(f"\nIngrese el codigo del producto: ")
         if codigo in tienda:
             print("codigo ya utilizado")
+        else:
+            break
+
+nombre = input("Nombre del Producto")
+categoria = input("categoria(hombre,mujer,niño)")
